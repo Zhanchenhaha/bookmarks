a7d6a5bdf770259e8262a6fa4a106be5ef07e8d9
Zhanchenhaha <1195241621@qq.com>
2020-05-14 15:58:10 +0800

    扩展用户模型

--- account/views.py
+++ account/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
-from account.forms import LoginForm, UserRegistrationForm
+from account.forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 
 
 def user_login(request):
@@ -48,7 +50,27 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # 保存User对象
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    try:
+        profile = request.user.profile
+    except ObjectDoesNotExist:
+        profile = Profile(user=request.user)
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(isinstance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form':  profile_form})
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -124,4 +124,5 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
